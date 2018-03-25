52fad74d5eed75ac8871189fc58c055c06c5c3c3
Eeva-Jonna <eeva-jonna.panula@w3.fi>
2018-03-25 21:17:49 +0300

    Refactor files from into mapping, add error handling to getDirs

--- src/md-to-pdf.js
+++ src/md-to-pdf.js
@@ -8,28 +8,31 @@ const convertToPdf = (dirFrom, dirTo, cssPath) => {
 }
 
 const getDirs = (dirFrom, dirTo) => {
-  const filesFrom = [];
-  fs.readdirSync(dirFrom).forEach(file => {
-      filesFrom.push(dirFrom + '/' + file);
-  });
+  try {
+    const filesFrom = fs.readdirSync(dirFrom)
+      .map(file => dirFrom + '/' + file);
 
-  const filesTo = filesFrom.map(file => {
-    let newFile = file.replace('.md', '.pdf').replace(dirFrom, dirTo);
-    return newFile;
-  });
+    const filesTo = filesFrom.map(file => {
+      let newFile = file.replace('.md', '.pdf').replace(dirFrom, dirTo);
+      return newFile;
+    });
 
-  return {
-    filesFrom,
-    filesTo
-  };
+    return {
+      filesFrom,
+      filesTo
+    };
+  } catch (e) {
+    console.error('Error in getDirs ', e);
+    process.exit(1);
+  }
 }
 
 const handleConvert = (filesFrom, filesTo, cssPath) => {
   const options = {
     cssPath: cssPath ? cssPath : path.resolve(__dirname, './pdf.css'),
     remarkable: {
-        html: true,
-        breaks: true,
+      html: true,
+      breaks: true,
     }
   }
 
